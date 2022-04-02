24abcc97daf11dac845719d792ab1fa2bc1cb856
Paulo <levchenkopa@it2g.ru>
2022-04-02 19:16:45 +0300

    добавил файл scripts.py с методами корректировки журнала. Внесены описания методов в README.md

---
+++ scripts.py
@@ -0,0 +1,53 @@
+import random
+from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
+
+
+def find_schoolkid_by_name(schoolkid_name):
+    schoolkids = Schoolkid.objects.filter(full_name__contains=schoolkid_name)
+    if len(schoolkids) == 1:
+        return schoolkids[0]
+    elif len(schoolkids) == 0:
+        print(f'Не удалось найти ученика с именем {schoolkid_name}')
+        return False
+    elif len(schoolkids) > 1:
+        full_names = [kid.full_name for kid in schoolkids]
+        print(f'Нашлось больше одного ученика: {full_names}')
+        return False
+
+
+def fix_marks(schoolkid_name):
+    schoolkid = find_schoolkid_by_name(schoolkid_name)
+    if schoolkid:
+        bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+        for bad_mark_from_filter in bad_marks:
+            bad_mark = Mark.objects.get(pk=bad_mark_from_filter.pk)
+            bad_mark.points = random.choice([4, 5])
+            bad_mark.save()
+
+
+def remove_chastisements(schoolkid_name):
+    schoolkid = find_schoolkid_by_name(schoolkid_name)
+    if schoolkid:
+        bad_opinions = Chastisement.objects.filter(schoolkid=schoolkid)
+        bad_opinions.delete()
+
+
+def create_commendation(schoolkid_name, subject_name):
+    schoolkid = find_schoolkid_by_name(schoolkid_name)
+    if schoolkid:
+        kid_lessons_on_subject = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
+                                                       group_letter=schoolkid.group_letter,
+                                                       subject__title=subject_name)
+        if kid_lessons_on_subject:
+            choice_list = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!']
+            text = random.choice(choice_list)
+            lesson = random.choice(kid_lessons_on_subject)
+            Commendation.objects.create(text=text, created=lesson.date, schoolkid=schoolkid, subject=lesson.subject,
+                                        teacher=lesson.teacher)
+        else:
+            print(f'Предмета с названием "{subject_name}" у ученика {schoolkid.full_name} не удалось найти!')
+            lessons = Lesson.objects.select_related('subject').filter(year_of_study=schoolkid.year_of_study,
+                                                                      group_letter=schoolkid.group_letter)
+            lessons = lessons.values('subject__title').distinct()
+            lessons_names = [[lesson[i] for i in lesson][0] for lesson in lessons if lesson]
+            print(f'Вот полный список предметов для {schoolkid.full_name}:\n{lessons_names}')
