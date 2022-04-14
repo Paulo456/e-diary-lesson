aeb42a473fcdc2419cfef6e77ab27824bf5d8a1f
Paulo <levchenkopa@it2g.ru>
2022-04-14 21:08:12 +0300

    убрал не нужные if после исправления find_schoolkid_by_name

--- scripts.py
+++ scripts.py
@@ -16,38 +16,35 @@ def find_schoolkid_by_name(schoolkid_name):
 
 def fix_marks(schoolkid_name):
     schoolkid = find_schoolkid_by_name(schoolkid_name)
-    if schoolkid:
-        bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-        for bad_mark_from_filter in bad_marks:
-            bad_mark = Mark.objects.get(pk=bad_mark_from_filter.pk)
-            bad_mark.points = random.choice([4, 5])
-            bad_mark.save()
+    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+    for bad_mark_from_filter in bad_marks:
+        bad_mark = Mark.objects.get(pk=bad_mark_from_filter.pk)
+        bad_mark.points = random.choice([4, 5])
+        bad_mark.save()
 
 
 def remove_chastisements(schoolkid_name):
     schoolkid = find_schoolkid_by_name(schoolkid_name)
-    if schoolkid:
-        bad_opinions = Chastisement.objects.filter(schoolkid=schoolkid)
-        bad_opinions.delete()
+    bad_opinions = Chastisement.objects.filter(schoolkid=schoolkid)
+    bad_opinions.delete()
 
 
 def create_commendation(schoolkid_name, subject_name):
     schoolkid = find_schoolkid_by_name(schoolkid_name)
-    if schoolkid:
-        kid_lessons_on_subject = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
-                                                       group_letter=schoolkid.group_letter,
-                                                       subject__title=subject_name)
-        if kid_lessons_on_subject:
-            choice_list = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!']
-            text = random.choice(choice_list)
-            lesson = random.choice(kid_lessons_on_subject)
-            Commendation.objects.create(text=text, created=lesson.date, schoolkid=schoolkid, subject=lesson.subject,
-                                        teacher=lesson.teacher)
-        else:
-            print(f'Предмета с названием "{subject_name}" у ученика {schoolkid.full_name} не удалось найти!')
-            lessons = Lesson.objects.select_related('subject').filter(year_of_study=schoolkid.year_of_study,
-                                                                      group_letter=schoolkid.group_letter)
-            lessons = lessons.values('subject__title').distinct()
-            lessons_names = [[lesson[i] for i in lesson][0] for lesson in lessons if lesson]
-            print(f'Вот полный список предметов для {schoolkid.full_name}:\n{lessons_names}')
+    kid_lessons_on_subject = Lesson.objects.filter(year_of_study=schoolkid.year_of_study,
+                                                   group_letter=schoolkid.group_letter,
+                                                   subject__title=subject_name)
+    if kid_lessons_on_subject:
+        choice_list = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!']
+        text = random.choice(choice_list)
+        lesson = random.choice(kid_lessons_on_subject)
+        Commendation.objects.create(text=text, created=lesson.date, schoolkid=schoolkid, subject=lesson.subject,
+                                    teacher=lesson.teacher)
+    else:
+        print(f'Предмета с названием "{subject_name}" у ученика {schoolkid.full_name} не удалось найти!')
+        lessons = Lesson.objects.select_related('subject').filter(year_of_study=schoolkid.year_of_study,
+                                                                  group_letter=schoolkid.group_letter)
+        lessons = lessons.values('subject__title').distinct()
+        lessons_names = [[lesson[i] for i in lesson][0] for lesson in lessons if lesson]
+        print(f'Вот полный список предметов для {schoolkid.full_name}:\n{lessons_names}')
 
